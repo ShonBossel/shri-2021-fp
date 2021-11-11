3f17b573ec8863d250ba5beaa782b8a0316eea22
ShonBossel <ferrari2535@mail.ru>
2021-11-11 14:54:04 +0300

    Added necessary functions in processSequence.js, realized required processSequence()

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -15,37 +15,92 @@
  * Ответ будет приходить в поле {result}
  */
 import Api from '../tools/api';
+const {allPass, andThen, compose, gt, length, lt, not, prop, tap, test, when} = require('ramda');
+
 
 const api = new Api();
 
-/**
- * Я – пример, удали меня
- */
-const wait = time => new Promise(resolve => {
-    setTimeout(resolve, time);
-})
+//Functions that get object fields
+const getValue = prop('value');
+const getWriteLog = prop('writeLog');
+const getHandleSuccess = prop('handleSuccess');
+const getHandleError = prop('handleError');
+const getResultData = prop('result');
 
-const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-    /**
-     * Я – пример, удали меня
-     */
-    writeLog(value);
+//Functions that compare numbers
+const isMoreThanTwo = lt(2);
+const isLessThanTen = gt(10);
+const isPositive = lt(0);
 
-    api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-        writeLog(result);
-    });
+//Functions that check input value
+const checkInputValueLength = allPass([isLessThanTen, isMoreThanTwo]);
+const isInputValueLengthValid = compose(
+    checkInputValueLength,
+    length
+);
+const checkInputValueNumber = test(/^\d+(\.\d+)?$/);
+const checkInputValueValid = allPass([isInputValueLengthValid, isPositive, checkInputValueNumber]);
+const isInputValueValid = compose(
+    checkInputValueValid,
+    getValue
+);
+const isInputValueNotValid = compose(
+    not,
+    isInputValueValid
+);
 
-    wait(2500).then(() => {
-        writeLog('SecondLog')
+//Functions that make math operatons
+const getMathRoundValue = compose(
+    Math.round,
+    parseFloat,
+    getValue
+);
+const getApiTech = api.get('https://api.tech/numbers/base');
+const getBinaryFromDecimal = (inputValue) => getApiTech({from: 10, to: 2, number: inputValue});
+const getPowNumber = (inputValue) => Math.pow(inputValue, 2);
+const getRemainderOfDivisionByThree = (inputValue) => inputValue % 3;
 
-        return wait(1500);
-    }).then(() => {
-        writeLog('ThirdLog');
+//Additional functions
+const logInputError = (inputData) => getHandleError(inputData)('ValidationError');
+const getAnimalById = (id) => api.get(`https://animals.tech/${id}`)({});
 
-        return wait(400);
-    }).then(() => {
-        handleSuccess('Done');
-    });
+//Main block
+const processSequence = (inputData) => {
+    const writeLogData = getWriteLog(inputData);
+    const getSuccessData = getHandleSuccess(inputData);
+    const logData = compose(
+        writeLogData,
+        getValue
+    );
+
+    const result = compose(
+        when(
+            isInputValueValid,
+            compose(
+                andThen(getSuccessData),
+                andThen(getResultData),
+                andThen(getAnimalById),
+                andThen(tap(writeLogData)),
+                andThen(getRemainderOfDivisionByThree),
+                andThen(tap(writeLogData)),
+                andThen(getPowNumber),
+                andThen(tap(writeLogData)),
+                andThen(length),
+                andThen(tap(writeLogData)),
+                andThen(getResultData),
+                getBinaryFromDecimal,
+                tap(writeLogData),
+                getMathRoundValue
+            )
+        ),
+        when(
+            isInputValueNotValid,
+            tap(logInputError)
+        ),
+        tap(logData)
+    );
+
+    result(inputData);
 }
 
 export default processSequence;
